1f803eff68b3927553bf12d85506cdb2982fc2d7
quotto <5211139+quotto@users.noreply.github.com>
2026-03-01 15:12:08 +0900

    feat: add API Gateway deployment resources for v1 and v2 stages

--- infra/api/stage/v2.tf
+++ infra/api/stage/v2.tf
@@ -74,11 +74,7 @@ resource "aws_lambda_alias" "throwtrash-update-alias-v2" {
     function_version = var.update_lambda["version"]
 }
 
-resource "aws_api_gateway_deployment" "api-deployment-prod" {
-    rest_api_id = var.api_gateway["api_id"]
-}
-
-resource "aws_api_gateway_base_path_mapping" "api-base-path-mapping" {
+resource "aws_api_gateway_base_path_mapping" "api-base-path-mapping-v2" {
   api_id      = var.api_gateway.api_id
   stage_name  = aws_api_gateway_stage.api-stage-prod-v2.stage_name
   domain_name = aws_api_gateway_domain_name.api-domain-name.domain_name
